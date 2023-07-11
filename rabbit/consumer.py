910656bc79f275ab19fd4a270fac9e78d3952c47
artem-senkov <119967327+artem-senkov@users.noreply.github.com>
2023-07-11 18:19:15 +0300

    1

---
+++ rabbit/consumer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+import pika
+connection =
+pika.BlockingConnection(pika.ConnectionParameters('192.168.0.141'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+def callback(ch, method, properties, body):
+ print(" [x] Received %r" % body)
+channel.basic_consume(queue='hello', on_message_callback=callback,
+auto_ack=False)
+channel.start_consuming()
---
+++ rabbit/producer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+import pika
+connection =
+pika.BlockingConnection(pika.ConnectionParameters('192.168.0.141'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello
+Netology!')
+connection.close()

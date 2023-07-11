4e73e2c6423e758f140e0b62c3d0842063117551
artem-senkov <119967327+artem-senkov@users.noreply.github.com>
2023-07-11 21:04:52 +0300

    Update consumer.py

--- rabbit/consumer.py
+++ rabbit/consumer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
 import pika
-connection =
-pika.BlockingConnection(pika.ConnectionParameters('192.168.0.141'))
+credentials = pika.PlainCredentials('test', 'test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('machine1',5672,'/',credentials))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
-def callback(ch, method, properties, body):
- print(" [x] Received %r" % body)
-channel.basic_consume(queue='hello', on_message_callback=callback,
-auto_ack=False)
+channel.queue_declare(queue='QueueAVS')
+def callback(ch, method, properties, body): print(" [x] Received %r" % body)
+channel.basic_consume(queue='QueueAVS', on_message_callback=callback,auto_ack=False)
 channel.start_consuming()
